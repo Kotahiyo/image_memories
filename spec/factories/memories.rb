1b7aa52cd19dea2939115a8ffd3fa7958ae08000
Kouhei <hiyotihakou@gmail.com>
2020-12-11 09:56:18 +0900

    post モデルと memories モデルを作成

---
+++ spec/factories/memories.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: memories
+#
+#  id         :bigint           not null, primary key
+#  image      :json
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint           not null
+#
+# Indexes
+#
+#  index_memories_on_post_id  (post_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#
+FactoryBot.define do
+  factory :memorie do
+    image { "" }
+    post { nil }
+  end
+end
