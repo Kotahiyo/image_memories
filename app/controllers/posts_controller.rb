0f1e3c1b2602f233008f2b36916b6a9691377e06
Kouhei <hiyotihakou@gmail.com>
2021-01-18 00:42:25 +0900

    検索機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
-    @posts = Post.all.includes(:user).order(id: "DESC")
+    @posts = Post.all.includes(:user).order(id: "DESC").search(params[:search])
   end
 
   def show
