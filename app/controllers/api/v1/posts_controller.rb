b1f6eb1bdb7ba87d993776681591eee2f3aace9a
Kouhei <hiyotihakou@gmail.com>
2021-03-21 22:44:27 +0900

    フォーマットを json に変更

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class PostsController < ApplicationController
-      before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+      # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
       def index
         @posts = Post.all.includes(:user).order(id: "DESC").page(params[:page]).per(8)
         @posts = @posts.search(params[:search]) if params[:search]
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root to: "api/v1/posts#index"
   namespace 'api' do
     namespace 'v1' do
-      resources :posts
+      resources :posts, defaults: { format: :json }
     end
   end
 
