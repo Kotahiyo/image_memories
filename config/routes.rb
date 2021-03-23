d06bec53bf3131d551d795f87dca3912e7c551b9
Kouhei <hiyotihakou@gmail.com>
2021-03-23 23:52:25 +0900

    削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root to: "api/v1/posts#index"
   namespace "api" do
     namespace "v1" do
-      resources :posts, defaults: { format: :json }
+      resources :posts
     end
   end
 
