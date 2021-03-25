3e24eaf4468fe08c1d8c80d4ef3b146f32292478
Kouhei <hiyotihakou@gmail.com>
2021-03-25 23:49:48 +0900

    Vue.js でアプリケーションを表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <ArticleList/>
   </div>
 </template>
 
 <script>
+import ArticleList from "./components/ArticleList"
 export default {
+  components: {
+    ArticleList
+  },
   data: function () {
     return {
       message: "Hello Vue!"
---
+++ app/javascript/components/ArticleList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="posts_wrapper__list">
+
+        <div class="posts_wrapper__list_wrap">
+          <div class="post">
+            <div class="post__top_box">
+              <span class="post__top_box__name">
+                投稿者:
+              </span>
+              <span class="post__top_box__time">
+                投稿日時：
+              </span>
+            </div>
+
+
+            <div class="post__midle_box_title">
+              タイトル:
+            </div>
+
+            <div class="post__under_box_admin">
+
+            </div>
+          </div>
+        </div>
+
+    <div class="posts_wrapper__list_paginate">
+
+    </div>
+
+  </div>
+
+</template>
